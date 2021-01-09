4063fe4319aeebfe83dd5c7f1048b59cf6de7914
froggagul <froggagul@gmail.com>
2021-01-09 16:14:02 +0900

    minimal change on rightnav

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+// export { default as ColorSelector } from './colorSelector';
+// export { default as Header } from './header';
+// export { default as LayerChoice } from './layerChoice';
+// export { default as LeftNav } from './leftnav';
+// export { default as MainBoard } from './mainBoard';
+// export { default as RightNav } from './rightnav';
+// export { default as UserStatus } from './userStatus';
