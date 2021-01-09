d85825eb665d0e95764e195fe947ef09660cbd97
bgeunjo <airmancho@naver.com>
2021-01-09 11:46:50 +0900

    헤더 적용

---
+++ src/styles/globalstyles.jsx
@@ -0,0 +1,11 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0px;
+  }
+  @font-face { font-family: 'ThecircleM'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/ThecircleM.woff') format('woff'); 
+  font-weight: normal; font-style: normal; }
+`;
+
+export default GlobalStyle;
